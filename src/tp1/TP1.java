59e426e9953b2353ea89d35e04589e38a12ea09f
celoire <celoire@pcalb-eb0202.univ-jfc.fr>
2019-09-17 14:44:20 +0200

    Changement du main

--- src/tp1/TP1.java
+++ src/tp1/TP1.java
@@ -16,7 +16,7 @@ public class TP1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // Ton main à rien dedans il faut écrire du code
     }
     
 }
